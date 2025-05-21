d504d3670066e7d8c1a022862438e3bd9496ee40
pachuho <juho7410@gmail.com>
2025-05-21 16:42:44 +0900

    feat: targetSDK 지정

--- build-logic/src/main/kotlin/benchmark.android.application.gradle.kts
+++ build-logic/src/main/kotlin/benchmark.android.application.gradle.kts
@@ -1,9 +1,14 @@
+import com.android.build.api.dsl.ApplicationExtension
 import com.pachuho.benchmark.configureComposeAndroid
 import com.pachuho.benchmark.configureHiltAndroid
 import com.pachuho.benchmark.configureKotlinAndroid
 
 plugins {
     id("com.android.application")
+}
+
+extensions.configure<ApplicationExtension> {
+    defaultConfig.targetSdk = 35
 }
 
 configureKotlinAndroid()
--- build-logic/src/main/kotlin/benchmark.android.library.gradle.kts
+++ build-logic/src/main/kotlin/benchmark.android.library.gradle.kts
@@ -1,10 +1,16 @@
+import com.android.build.gradle.LibraryExtension
 import com.pachuho.benchmark.configureCoroutineAndroid
 import com.pachuho.benchmark.configureHiltAndroid
 import com.pachuho.benchmark.configureKotlinAndroid
+import org.gradle.kotlin.dsl.configure
 
 plugins {
     id("com.android.library")
     id("benchmark.verify.detekt")
+}
+
+extensions.configure<LibraryExtension> {
+    defaultConfig.targetSdk = 35
 }
 
 configureKotlinAndroid()
