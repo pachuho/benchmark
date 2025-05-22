d5d62cd0d0ba6fb1361482f15585ba35f2364df0
pachuho <juho7410@gmail.com>
2025-05-22 22:22:23 +0900

    feat: build-logic 수준의 feature 종속성 추가

--- build-logic/src/main/kotlin/benchmark.android.feature.gradle.kts
+++ build-logic/src/main/kotlin/benchmark.android.feature.gradle.kts
@@ -8,6 +8,36 @@ plugins {
     id("benchmark.android.compose")
 }
 
+android {
+    packaging {
+        resources {
+            excludes.add("META-INF/**")
+        }
+    }
+    defaultConfig {
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+}
+
 configureHiltAndroid()
 configureKotlinAndroid()
 configureComposeAndroid()
+
+dependencies {
+    implementation(project(":core:model"))
+    implementation(project(":core:data"))
+    implementation(project(":core:designsystem"))
+    implementation(project(":core:domain"))
+    implementation(project(":core:navigation"))
+    implementation(project(":core:ui"))
+
+    testImplementation(project(":core:testing"))
+
+    val libs = project.extensions.libs
+    implementation(libs.findLibrary("hilt.navigation.compose").get())
+    implementation(libs.findLibrary("androidx.compose.navigation").get())
+    androidTestImplementation(libs.findLibrary("androidx.compose.navigation.test").get())
+
+    implementation(libs.findLibrary("androidx.lifecycle.viewModelCompose").get())
+    implementation(libs.findLibrary("androidx.lifecycle.runtimeCompose").get())
+}
