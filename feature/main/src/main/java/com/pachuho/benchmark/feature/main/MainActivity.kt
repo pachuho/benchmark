a04c4c3d0bdb3fcc723e5d203063a708c5f38c0c
pachuho <juho7410@gmail.com>
2025-05-29 01:18:21 +0900

    feat: 연속 뒤로가기 입력 시 앱 종료 처리

--- feature/main/src/main/java/com/pachuho/benchmark/feature/main/MainActivity.kt
+++ feature/main/src/main/java/com/pachuho/benchmark/feature/main/MainActivity.kt
@@ -1,19 +1,26 @@
 package com.pachuho.benchmark.feature.main
 
+import android.app.Activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.BackHandler
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import com.pachuho.benchmark.core.designsystem.theme.BenchmarkTheme
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 import java.net.UnknownHostException
 
@@ -46,6 +53,8 @@ class MainActivity : ComponentActivity() {
             }
 
             BenchmarkTheme {
+                BackOnPressed(snackBarHostState, coroutineScope)
+
                 Scaffold(modifier = Modifier,
                     content = { padding ->
                         MainNavHost(
@@ -57,6 +66,29 @@ class MainActivity : ComponentActivity() {
                     snackbarHost = { SnackbarHost(snackBarHostState) }
                 )
             }
+        }
+    }
+
+    @Composable
+    fun BackOnPressed(
+        snackBarHostState: SnackbarHostState,
+        scope: CoroutineScope
+    ) {
+        val context = LocalContext.current
+        var backPressedState by remember { mutableStateOf(true) }
+        var backPressedTime = 0L
+
+        BackHandler(enabled = backPressedState) {
+            if(System.currentTimeMillis() - backPressedTime <= 1_000) {
+                (context as Activity).finish()
+            } else {
+                backPressedState = true
+
+                scope.launch {
+                    snackBarHostState.showSnackbar("한번 더 누르면 앱이 종료됩니다.")
+                }
+            }
+            backPressedTime = System.currentTimeMillis()
         }
     }
 }
