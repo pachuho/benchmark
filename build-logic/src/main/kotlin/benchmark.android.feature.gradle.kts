d2c3a2674b49f51d2a122d68e8a12758ae9871a7
pachuho <juho7410@gmail.com>
2025-06-26 15:18:32 +0900

    feat: feature 모듈에서 data 모듈을 바라보지 않도록 수정

--- build-logic/src/main/kotlin/benchmark.android.feature.gradle.kts
+++ build-logic/src/main/kotlin/benchmark.android.feature.gradle.kts
@@ -25,7 +25,6 @@ configureComposeAndroid()
 
 dependencies {
     implementation(project(":core:model"))
-    implementation(project(":core:data"))
     implementation(project(":core:designsystem"))
     implementation(project(":core:domain"))
     implementation(project(":core:navigation"))
